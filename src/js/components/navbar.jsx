de609cdc9eb67582851de1bb098aeb0b5418851c
manuellosadat2-a11y <manuellosadat2@gmail.com>
2026-01-19 23:19:56 +0000

    react exercise

--- src/js/components/navbar.jsx
+++ src/js/components/navbar.jsx
@@ -2,20 +2,32 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg bg-dark">
-			<div className="container">
-				<a className="navbar-brand text-white" href="#">
-					Start Bootstrap
-				</a>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#">Start Bootstrap</a>
 
-				<div className="navbar-nav ms-auto">
-					<a className="nav-link text-white" href="#">Home</a>
-					<a className="nav-link text-white" href="#">About</a>
-					<a className="nav-link text-white" href="#">Services</a>
-					<a className="nav-link text-white" href="#">Contact</a>
-				</div>
-			</div>
-		</nav>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav ms-auto">
+            <a className="nav-link active" href="#">Home</a>
+            <a className="nav-link" href="#">About</a>
+            <a className="nav-link" href="#">Services</a>
+            <a className="nav-link" href="#">Contact</a>
+          </div>
+        </div>
+      </div>
+    </nav>
 	);
 };
 
